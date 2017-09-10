9c148b7842784f76fdb1b6a0cd943b2f1ac19b52
Eric <ericou@gmail.com>
2017-09-10 10:16:29 +0800

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,11 @@
 
 ## Write a short comment describing this function
 
+## This function creates a list containing four functions:
+## 1. set(): Set a matrix
+## 2. get(): Get the matrix
+## 3. setinv(): Set the inversed matrix
+## 4. getinv(): Get the inversed matrix
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -20,6 +25,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+## This function returns the inversed matrix. It first checks if the inversion
+## has been solved. If so, it gets the inversed matrix from the cache and skips
+## the computation. Otherwise, it solves the inversed matrix and sets it to the 
+## cache by setinv() function.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     i <- x$getinv()
